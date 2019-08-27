9df54f524caffd517a61cdcd36bca89903e895f1
paperbenni <paperbenni@gmail.com>
2019-08-27 17:17:51 +0200

    updated ngrok

--- colab.sh
+++ colab.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 source <(curl -s https://raw.githubusercontent.com/paperbenni/bash/master/import.sh)
 pb ngrok
+rungrok http 8443 --log=stdout &
 curl -sSL https://github.com/cdr/code-server/releases/download/1.1119-vsc1.33.1/code-server1.1119-vsc1.33.1-linux-x64.tar.gz | tar -xvz
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &> /dev/null
-unzip ngrok-stable-linux-amd64.zip
-./ngrok http 8443 --log=stdout &> /dev/null & ./code-server1.1119-vsc1.33.1-linux-x64/code-server --allow-http --no-auth / &> /dev/null &
+./code-server1.1119-vsc1.33.1-linux-x64/code-server --allow-http --no-auth
 echo "code server adress: "
 getgrok http
 getgrok https
